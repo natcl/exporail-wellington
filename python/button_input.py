e7366292b1ac5ac7391554ed79d89b44b8f4f62f
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-09-30 17:41:08 -0400

    Added threading

--- python/button_input.py
+++ python/button_input.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
+from wellington import wellingtonPlayer
 
 button1 = 4
 button2 = 17 
 button3 = 27
 button4 = 22
 
+wp = wellingtonPlayer()
 
 def on_button1(channel):
     print 'Button1 pressed'
+    wp.start('parcours1.json')
 def on_button2(channel):
     print 'Button2 pressed'
+    wp.stop()
 def on_button3(channel):
     print 'Button3 pressed'
 def on_button4(channel):
@@ -23,8 +27,7 @@ GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(button1, GPIO.RISING, callback=on_button1, bouncetime=1000)
-GPIO.add_event_detect(button2, GPIO.RISING, callback=on_button2, bouncetime=200)
-GPIO.add_event_detect(button3, GPIO.RISING, callback=on_button3, bouncetime=200)
-GPIO.add_event_detect(button4, GPIO.RISING, callback=on_button4, bouncetime=200)
-
+GPIO.add_event_detect(button2, GPIO.RISING, callback=on_button2, bouncetime=1000)
+GPIO.add_event_detect(button3, GPIO.RISING, callback=on_button3, bouncetime=1000)
+GPIO.add_event_detect(button4, GPIO.RISING, callback=on_button4, bouncetime=1000)
 
--- python/wellington.py
+++ python/wellington.py
@@ -2,75 +2,96 @@ import serial
 import time
 import json
 import sys
+import threading
 
-if sys.platform == 'linux2':
-    mega1_port = '/dev/ttyACM0'
-    mega2_port = '/dev/ttyACM1'
-if sys.platform == 'darwin':
-    mega1_port = '/dev/tty.usbmodem1421'
-    mega2_port = '/dev/tty.usbmodem1411'
+class wellingtonPlayer(threading.Thread):
+    def __init__(self):
 
-mega1 = serial.Serial(mega1_port, 115200)
-mega2 = serial.Serial(mega2_port, 115200)
-time.sleep(5)
+        if sys.platform == 'linux2':
+            mega1_port = '/dev/ttyACM0'
+            mega2_port = '/dev/ttyACM1'
+        if sys.platform == 'darwin':
+            mega1_port = '/dev/tty.usbmodem1421'
+            mega2_port = '/dev/tty.usbmodem1411'
+        self.playing = False
+        self._stop = False
+        
+        try:
+            self.mega1 = serial.Serial(mega1_port, 115200)
+            self.mega2 = serial.Serial(mega2_port, 115200)
+        except serial.serialutil.SerialException:
+            print "Serial device does not exist, writing to file instead."
+            self.mega1 = open('mega1.txt', 'w')
+            self.mega2 = open('mega2.txt', 'w')
 
-with open('pin_mapping.json') as f:
-    pin_mapping = json.loads(f.read())
+        time.sleep(5)
 
-def off():
-    mega1.write(chr(255)+chr(3))
-    mega2.write(chr(255)+chr(3))
+        with open('pin_mapping.json') as f:
+            self.pin_mapping = json.loads(f.read())
 
-def close():
-    off()
-    time.sleep(5)
+    def off(self):
+        self.mega1.write(chr(255)+chr(3))
+        self.mega2.write(chr(255)+chr(3))
 
-    mega1.close()
-    time.sleep(2)
-    mega2.close()
+    def close(self):
+        self.off()
+        time.sleep(5)
 
-def event(filename, event_number):
-    with open(filename) as f:
-        parcours = json.loads(f.read())
-    for light in parcours1[str(event_number)]:
-        arduino = pin_mapping[str(light[0])][0]
-        pin = pin_mapping[str(light[0])][1]
-        state = light[1]
-        if arduino == 1:
-            mega1.write(chr(pin)+chr(state))
-        if arduino == 2:
-            mega2.write(chr(pin)+chr(state))
+        self.mega1.close()
+        time.sleep(2)
+        self.mega2.close()
 
-def light(light_number):
-    off()
-    arduino = pin_mapping[str(light_number)][0]
-    pin = pin_mapping[str(light_number)][1]
-    if arduino == 1:
-        mega1.write(chr(pin)+chr(1))
-    if arduino == 2:
-        mega2.write(chr(pin)+chr(1))
+    def play_event(self, filename, event_number):
+        with open(filename) as f:
+            parcours = json.loads(f.read())
+        self.event(parcours, event_number)
 
-def play(filename, speed = 1):
-    with open(filename) as f:
-        parcours = json.loads(f.read())
-    
-    for event in sorted([int(key) for key in parcours.keys()]):
-        print event
-        for light in parcours[str(event)]:
-            arduino = pin_mapping[str(light[0])][0]
-            pin = pin_mapping[str(light[0])][1]
+
+    def event(self, parcours, event_number):
+        for light in parcours[str(event_number)]:
+            arduino = self.pin_mapping[str(light[0])][0]
+            pin = self.pin_mapping[str(light[0])][1]
             state = light[1]
             if arduino == 1:
-                mega1.write(chr(pin)+chr(state))
+                self.mega1.write(chr(pin)+chr(state))
             if arduino == 2:
-                mega2.write(chr(pin)+chr(state))
-        time.sleep(speed)
+                self.mega2.write(chr(pin)+chr(state))
+            if light[1] == 0:
+                print('\tLight {0} off'.format(light[0]))
+            if light[1] == 1:
+                print('\tLight {0} on'.format(light[0]))
+            if light[1] == 2:
+                print('\tLight {0} blink'.format(light[0]))
+            if light[1] == 3:
+                print('All off')
 
-def playloop(filename, speed = 1):
-    while True:
-        play(filename, speed)
 
-if len(sys.argv) > 1:
-    play(sys.argv[1])
-    close()
+    def light(self, light_number):
+        self.off()
+        arduino = self.pin_mapping[str(light_number)][0]
+        pin = self.pin_mapping[str(light_number)][1]
+        if arduino == 1:
+            self.mega1.write(chr(pin)+chr(1))
+        if arduino == 2:
+            self.mega2.write(chr(pin)+chr(1))
+
+    def start(self, filename, speed = 1):
+        if not self.playing:
+            self.playing = True
+            with open(filename) as f:
+                parcours = json.loads(f.read())
+        
+            for event in sorted([int(key) for key in parcours.keys()]):
+                print('Event {0}'.format(event))
+                self.event(parcours, event)
+                time.sleep(speed)
+                if self._stop:
+                    break;
+            self.playing = False
+            self._stop = False
+            print 'Exited thread'
+
+    def stop(self):
+        if self.playing:
+            self._stop = True
 
