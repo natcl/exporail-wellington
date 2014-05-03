66420d502df4fd2ff3339034be307d97df421884
Nathanaël Lécaudé <nat@studioimaginaire.com>
2014-05-03 11:04:06 -0400

    Work on buttons

--- python/button_input.py
+++ python/button_input.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import sys
 import RPi.GPIO as GPIO
 from wellington import wellingtonPlayer
 
@@ -16,18 +17,30 @@ def on_button1(channel):
 def on_button2(channel):
     print 'Button2 pressed'
     wp.stop()
+    wp.off()
 def on_button3(channel):
     print 'Button3 pressed'
 def on_button4(channel):
     print 'Button4 pressed'
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(button4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(button1, GPIO.RISING, callback=on_button1, bouncetime=1000)
 GPIO.add_event_detect(button2, GPIO.RISING, callback=on_button2, bouncetime=1000)
 GPIO.add_event_detect(button3, GPIO.RISING, callback=on_button3, bouncetime=1000)
 GPIO.add_event_detect(button4, GPIO.RISING, callback=on_button4, bouncetime=1000)
+
+if sys.flags.interactive:
+    pass
+else:
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        wp.stop()
+        wp.off()
+        print('Quitting')
 
